22563bf1c1fc11078ea9605f8938494cfd06399e
Leonardo23Mendez <lmendezm003@alumno.uaemex.com>
2021-05-14 09:20:13 -0500

    :sparkles: Estructura base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import './Socialbar.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<html>
+    <head>
+        <title>HOME</title>
+    </head>
+
+    <body>
+        <div id="contenedor">
+            <header>
+                <h1>BIENVENIDO A "RATATOUILLE"</h1>
+            </header>
+            <nav>
+                <h2>MENU</h2>
+            </nav>
+            <section id="contenido">
+                <article>
+                    <h2>CONTENIDO</h2>
+                </article>
+            </section>
+            <aside>
+                <h2>PUBLICIDAD</h2>
+
+                <div class="social-bar">
+                  <a href="https://www.facebook.com" class="icon icon-facebook" target="_blank"><img src= 'https://img-premium.flaticon.com/png/512/747/747374.png?token=exp=1620999070~hmac=6faaee245b33871f6f6a9da7ee73b329' width="20" height="20"></img></a>
+                  <a href="https://twitter.com" class="icon icon-twitter" target="_blank"><img src= 'https://img-premium.flaticon.com/png/512/747/747453.png?token=exp=1620999062~hmac=acead89585b49c1046adc3d0f5f882a8' width="20" height="20"></img></a>
+                  <a href="https://www.youtube.com" class="icon icon-youtube" target="_blank"><img src= 'https://img-premium.flaticon.com/png/512/747/747458.png?token=exp=1620999070~hmac=5100175640f6f18d7b82e5daad315fd7' width="20" height="20"></img></a>
+                  <a href="https://www.instagram.com" class="icon icon-instagram" target="_blank"><img src= 'https://img-premium.flaticon.com/png/512/747/747393.png?token=exp=1620999070~hmac=3df3e859923ce213eea5e0ad2c34e58f' width="20" height="20"></img></a>
+                </div>
+
+            </aside>
+            <footer>
+                <h2>PIE DE PAGINA</h2>
+            </footer>
+        </div>
+    </body>
+</html>
   );
 }
 
