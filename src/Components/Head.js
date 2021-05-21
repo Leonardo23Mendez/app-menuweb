1cdc1a6396eb507b380ca02d4e9b0abd87f55be3
Javier832 <79716063+Javier832@users.noreply.github.com>
2021-05-21 15:55:17 -0500

    :lips: Refactorizacion

---
+++ src/Components/Head.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App';
+
+export default function Head(){
+    return(
+        <header>
+            <h1>BIENVENIDO A "RATATOUILLE"</h1>
+        </header>
+    );
+};
---
+++ src/Components/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App';
+
+export default function Menu(){
+    return(
+        <nav>
+            <h2>MENU</h2>
+        </nav>
+    );
+};
