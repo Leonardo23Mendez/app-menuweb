237d3799a00e7504521cecba3cbd6cf8f8c8fb54
Javier832 <79716063+Javier832@users.noreply.github.com>
2021-05-21 22:01:57 -0500

    :lips: Ap1Qr

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Head from './Components/Head';
 import Contenido from './Components/Contenido';
 import Menu from './Components/Menu';
 import Foot from './Components/Foot';
+import Publicidad from './Components/Publicidad';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-<html>
+
+    return (
+    <html>
     <head>
         <title>HOME</title>
     </head>
@@ -20,8 +23,9 @@ function App() {
                 <Contenido></Contenido>
             </section>
             <aside>
+                <Publicidad></Publicidad>
                 <h2>PUBLICIDAD</h2>
-
+                
                 <div class="social-bar">
                   <a href="https://www.facebook.com" class="icon icon-facebook" target="_blank"><img src= 'https://img-premium.flaticon.com/png/512/747/747374.png?token=exp=1620999070~hmac=6faaee245b33871f6f6a9da7ee73b329' width="20" height="20"></img></a>
                   <a href="https://twitter.com" class="icon icon-twitter" target="_blank"><img src= 'https://img-premium.flaticon.com/png/512/747/747453.png?token=exp=1620999062~hmac=acead89585b49c1046adc3d0f5f882a8' width="20" height="20"></img></a>
@@ -35,5 +39,4 @@ function App() {
 </html>
 );
 }
-
 export default App;
