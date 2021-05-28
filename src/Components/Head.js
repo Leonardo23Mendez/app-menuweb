8a383c2f3fe08745fcfd267ef221c19b421d15a1
Leonardo23Mendez <lmendezm003@alumno.uaemex.com>
2021-05-28 00:32:34 -0500

    :lips:  rama-cambios-imagen

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -4,7 +4,7 @@ import '../App';
 export default function Head(){
     return(
         <header>
-            <h1>BIENVENIDO A "RATATOUILLE"</h1>
+            <img src= 'https://img-premium.flaticon.com/png/512/747/747374.png?token=exp=1620999070~hmac=6faaee245b33871f6f6a9da7ee73b329' width="100" height="100"></img>
         </header>
     );
 };
