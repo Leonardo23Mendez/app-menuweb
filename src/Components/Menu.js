cba1abdebb327dafee4262d9dab6eba562874da3
DiegoGl44 <diegogalvanl@hotmail.com>
2021-05-27 23:41:23 -0500

    :lips: aportacion1

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../App';
-
 export default function Menu(){
     return(
         <nav>
-            <h2>MENU</h2>
+            <div id="header">
+                <ul class="nav logo-nav-container">
+                    <li><a href="">DESAYUNO</a></li>
+                    <li><a href="">COMIDA</a></li>
+                </ul>
+            </div>
         </nav>
     );
 };
