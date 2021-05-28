1624edd836b7c1a363617b1adcee1ce4c23f11b6
Leonardo23Mendez <lmendezm003@alumno.uaemex.com>
2021-05-27 22:11:14 -0500

    :sparkles: Configuracion footer

--- src/Components/Foot.js
+++ src/Components/Foot.js
@@ -4,7 +4,13 @@ import '../App';
 export default function Foot(){
     return(
         <footer>
-            <h2>PIE DE PAGINA</h2>
+            <section class="ft-legal">
+                <ul class="ft-legal-list">
+                    <li><a href="https://www.patrimoniocultural.gob.cl/614/w3-article-29742.html?_noredirect=1">Terminos y Condiciones</a></li>
+                    <li><li><a href="https://www.ilo.org/global/privacy-policy/lang--es/index.htm">Politica de Privacidad</a></li></li>
+                    <li>&copy; RATATOUILLE</li>
+                </ul>
+            </section>        
         </footer>
     );
 };
